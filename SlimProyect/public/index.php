b12013f07010063b6dc9eaf53cd0604feff9eeb1
alexis Holguin <william.holguin@zettaflops.co>
2020-05-29 23:31:41 -0500

    agregado de cors

--- SlimProyect/public/index.php
+++ SlimProyect/public/index.php
@@ -1,16 +1,40 @@
 <?php
 
-require '../vendor/autoload.php';
+  require '../vendor/autoload.php';
 
-$configuration = [
+  $configuration = [
     'settings' => [
-        'displayErrorDetails' => true,
+      'displayErrorDetails' => true,
     ],
-];
-$c = new \Slim\Container($configuration);
-$app = new Slim\App($c);
+  ];
+  $c = new \Slim\Container($configuration);
+  $app = new Slim\App($c);
+
+
+  $app->add(
+    function ($request, $response, $next) {
+      $method = $request->getMethod();
+      if ($method === 'OPTIONS') {
+        $response = new Slim\Psr7\Response();
+        $response->getBody()->write('{"status": "ok"}');
+        return $response
+          ->withStatus(200)
+          ->withHeader('Content-Type', 'application/json; charset=utf-8')
+          ->withHeader('Access-Control-Allow-Origin', '*')
+          ->withHeader('access-control-allow-credentials', 'true')
+          ->withHeader('Access-Control-Allow-Headers', ' X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method')
+          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      } else {
+        $response = $next($request, $response);
+        return $response
+          ->withHeader('Content-Type', 'application/json; charset=utf-8')
+          ->withHeader('Access-Control-Allow-Origin', '*')
+          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      }
+    }
+  );
 
 // Automatically load routers files
-require_once '../src/routers/autoload.php';
+  require_once '../src/routers/autoload.php';
 
-$app->run();
+  $app->run();
